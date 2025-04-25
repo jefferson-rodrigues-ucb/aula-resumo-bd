c54fd574de15fad0b63f7632a81419b1abaed9ee
Jefferson-S-Rodrigues <jefferson2jsr@gmail.com>
2025-04-25 18:59:17 -0300

    Exercício para 25/04

---
+++ exercicios.sql
@@ -0,0 +1,27 @@
+-- rode o script alimentos.sql
+
+-- Responda
+
+-- 1) Qual a diferença entre bancos de dados Relacionais e NoSQL?
+
+-- 2) Com a chegada do NoSQL, faz sentido estudar bancos de dados relacionais?
+
+-- 3) Por que uma boa modelagem de dados ainda é importante?
+
+-- 4) Como contruir uma tabela no MySQL?
+
+-- 5) Para que serve e como funcionam os comandos INSERT, UPDATE e DELETE?
+
+-- 6) Acrescente mais dois DEPARTAMENTOs, com o campo DESCONTO > 0.
+
+-- 7) Acrescente FORNECEDORes para esses DEPARTAMENTOs.
+
+-- 8) Acrescente mais PRODUTOs, acrescente PRODUTOs para os novos DEPARTAMENTOs e para os antigos.
+
+-- 9) Apresente os produtos com quantidade baixa (menos que 10 unidades)
+
+-- 10) Apresente os produtos por ordem de preço, do mais barato ao mais caro
+
+-- 11) Qual produto que, se todas as unidades forem vendidas, dará mais lucro?
+
+-- 12) Apresente o lucro por departamento, considere o desconto e a taxa do fornecedor no seu cálculo.
